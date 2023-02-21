c56e4ab2911fe34ed546ab029e81184d3fece4d7
tty95 <laravel0905@gmail.com>
2023-02-21 17:25:54 +0900

    refactor:MemoのController、modelとgoogleログインのcontroller、usermodelのコードを修正した。 #31

--- app/Http/Controllers/GoogleLoginController.php
+++ app/Http/Controllers/GoogleLoginController.php
@@ -10,27 +10,27 @@ use Laravel\Socialite\Facades\Socialite;
 class GoogleLoginController extends Controller
 {
 
-    public function __construct(User $user)
-    {
-            $this->user = $user; //Usermodelをインスタンス化
-    }
+        public function __construct(User $user)
+        {
+                $this->user = $user; //Usermodelをインスタンス化
+        }
 
-    public function getGoogleAuth()
-    {
-            return Socialite::driver('google')
-                    ->redirect();
-    }
+        public function getGoogleAuth()
+        {
+                return Socialite::driver('google')
+                        ->redirect();
+        }
 
-    public function authGoogleCallback()
-    {
-            $gUser = Socialite::driver('google')->stateless()->user();
-            // email が合致するユーザーを取得
-            $user = $this->user->findEmail($gUser);
-            // 見つからなければ新しくユーザーを作成
-            if ($user === null) {
-                    $user = $this->user->createUserByGoogle($gUser);
-            }
-            Auth::login($user, true);
-            return redirect('/memopad');
-    }
+        public function authGoogleCallback()
+        {
+                $gUser = Socialite::driver('google')->stateless()->user();
+                // email が合致するユーザーを取得
+                $user = $this->user->findEmail($gUser);
+                // 見つからなければ新しくユーザーを作成
+                if ($user === null) {
+                        $user = $this->user->createUserByGoogle($gUser);
+                }
+                Auth::login($user, true);
+                return redirect('/memopad');
+        }
 }
